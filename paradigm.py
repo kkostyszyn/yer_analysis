dc681ad7d3aa5590ef5477e48e90b261d9761db2
kkostyszyn <kalinakostyszyn@gmail.com>
2021-09-13 14:00:19 -0400

    Adding to code.
    Basic processing to Unimorph style, to begin building paradigms. Next step is POL-->IPA transcription, which will lead to yer detection.

--- paradigm.py
+++ paradigm.py
@@ -6,32 +6,39 @@ class Paradigm:
 		Given a list of words, scan to determine if a yer is present. If
 		so, make the paradigm a pair of Feature objects to compare.
 	"""
-	def __init__(self, wrd: dict) -> None:
+	def __init__(self, wrd: str, forms) -> None:
 		"""
 		Compare the lemma form against all inflected forms to determine
 		if there is a yer. If so, create Features.
 		"""
-		if is_yer(A, B):
-			#Check has_yer, how to negate value.
-			has_yer, prefix = find_prefix(A, B)
-			self.lemma = Features(A, prefix, has_yer, True)
-			self.inf = Features(B, prefix, not has_yer)
-		return True
+		self.lemma = wrd
+		self.forms = {}
+		for x in forms:
+			self.forms[x[0]] = x[1]
+		#if is_yer(A, B):
+		#	#Check has_yer, how to negate value.
+		#	has_yer, prefix = find_prefix(A, B)
+		#	self.lemma = Features(A, prefix, has_yer, True)
+		#	self.inf = Features(B, prefix, not has_yer)
 
 	def __repr__(self):
 		return True
 		
 	def __str__(self):
-		return True
+		s = self.lemma +":"
+		for x in self.forms.keys():
+			s += "\t" + x +"//" + self.forms[x]
+		s += "\n"
+		return s 
 		
-	def is_yer(lem, inf) -> boolean:
+	def is_yer(lem, inf) -> bool:
 		"""
 		Take two forms to compare place-by-place to determine if a yet.
 		Must except suffixes. 
 		"""
 		return True 
 		
-	def find_prefix(lem: str, inf: str) -> (boolean, str):
+	def find_prefix(lem: str, inf: str) -> (bool, str):
 		"""
 		Given two strings, find the longest common prefix to mark where the yer occurs.
 		"""
@@ -46,7 +53,7 @@ class Features:
 		feature map, including phonological features, inflectional 
 		information, and syllable information.
 	"""
-	def __init__(self, wrd: str, prefix: str, yer:boolean, lemma=False) -> None:
+	def __init__(self, wrd: str, prefix: str, yer:bool, lemma=False) -> None:
 		"""
 		Given the word, its prefix (to mark where in the word the yer 
 		occurs), log morphophonological features.
