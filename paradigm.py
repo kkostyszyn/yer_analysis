d4ad72bba9a8859825a4752b0efb6d3045ebddaf
kkostyszyn <kalinakostyszyn@gmail.com>
2021-09-13 12:43:20 -0400

    Pushing Paradigm & Featural data structures.

--- paradigm.py
+++ paradigm.py
@@ -1,36 +1,76 @@
 #import re
 #import pynini
-import sys
 
-def main(f):
-    """
-    First, open Polish morphological data compiled by Unimorph. 
-    
-    Then, for all inflected forms, compare to the lemma to find inserted/deleted vowels - sign of yer. 
-    
-    See if there is a way to generalize across resulting patterns.
-    """
-    f = open(f)
-    f = f.readlines()
-    
-    for x in f:
-        temp = re.split("\s", x)
-        if not cur:
-            if lst:
-                paradigm(lst)
-                pass 
-            cur = temp[0]
-            lst = []
-        lst.append(temp[1])
-    #[0] = lemma
-    #[1] = inflected form 
-    return True
+class Paradigm:
+	"""
+		Given a list of words, scan to determine if a yer is present. If
+		so, make the paradigm a pair of Feature objects to compare.
+	"""
+	def __init__(self, wrd: dict) -> None:
+		"""
+		Compare the lemma form against all inflected forms to determine
+		if there is a yer. If so, create Features.
+		"""
+		if is_yer(A, B):
+			#Check has_yer, how to negate value.
+			has_yer, prefix = find_prefix(A, B)
+			self.lemma = Features(A, prefix, has_yer, True)
+			self.inf = Features(B, prefix, not has_yer)
+		return True
 
-def paradigm(lst):
-    """
-    For a list of inflected forms, find possible yers. 
-    """
-    return True 
+	def __repr__(self):
+		return True
+		
+	def __str__(self):
+		return True
+		
+	def is_yer(lem, inf) -> boolean:
+		"""
+		Take two forms to compare place-by-place to determine if a yet.
+		Must except suffixes. 
+		"""
+		return True 
+		
+	def find_prefix(lem: str, inf: str) -> (boolean, str):
+		"""
+		Given two strings, find the longest common prefix to mark where the yer occurs.
+		"""
+	
+		#return triple of whether A is yer-bearing and the prefix
+		#if A is not yer-bearing, 
+		return True
 
-if __name__=="__main__":
-    main(sys.argv[-1])
+class Features:
+	"""
+		Given a word, make note of the 
+		feature map, including phonological features, inflectional 
+		information, and syllable information.
+	"""
+	def __init__(self, wrd: str, prefix: str, yer:boolean, lemma=False) -> None:
+		"""
+		Given the word, its prefix (to mark where in the word the yer 
+		occurs), log morphophonological features.
+		"""
+		
+		#Morphology
+		self.gender = wrd["gender"]
+		self.case = wrd["case"]
+		
+		#Syllables
+		self.syllable = count_syl(prefix)
+		
+		#Phonology
+		
+		return True
+
+	def __repr__(self):
+		return True
+		
+	def __str__(self):
+		return True
+		
+	def count_syl(prefix: str) -> int:
+		"""
+		Counts the vowel/consonant alternations to determine number of syllables in prefix.
+		"""
+		return True
