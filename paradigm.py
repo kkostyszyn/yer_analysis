5819eabbf052950eec83389643fe4f7c6b793bc5
kkostyszyn <kalinakostyszyn@gmail.com>
2021-11-03 20:57:22 -0400

    Updating for new code

--- paradigm.py
+++ paradigm.py
@@ -6,20 +6,15 @@ class Paradigm:
 		Given a list of words, scan to determine if a yer is present. If
 		so, make the paradigm a pair of Feature objects to compare.
 	"""
-	def __init__(self, wrd: str, forms) -> None:
+	def __init__(self, wrd: str, lem_yer: bool, forms) -> None:
 		"""
 		Compare the lemma form against all inflected forms to determine
 		if there is a yer. If so, create Features.
 		"""
 		self.lemma = wrd
-		self.forms = {}
-		for x in forms:
-			self.forms[x[0]] = x[1]
-		#if is_yer(A, B):
-		#	#Check has_yer, how to negate value.
-		#	has_yer, prefix = find_prefix(A, B)
-		#	self.lemma = Features(A, prefix, has_yer, True)
-		#	self.inf = Features(B, prefix, not has_yer)
+		self.lem_yer = lem_yer
+		
+		
 
 	def __repr__(self):
 		return True
@@ -37,15 +32,6 @@ class Paradigm:
 		Must except suffixes. 
 		"""
 		return True 
-		
-	def find_prefix(lem: str, inf: str) -> (bool, str):
-		"""
-		Given two strings, find the longest common prefix to mark where the yer occurs.
-		"""
-	
-		#return triple of whether A is yer-bearing and the prefix
-		#if A is not yer-bearing, 
-		return True
 
 class Features:
 	"""
